878cc6abd1a418e6e6b77cb9dea1d1033949deac
davidnx01 <info@webdavid.sk>
2023-04-22 23:37:19 +0200

    MainCTA Section + Responsivity

--- common/Containers/MainCTA/MainCTA.jsx
+++ common/Containers/MainCTA/MainCTA.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./mainCTA.css"
+import CTA from '../../Components/CTA/CTA'
 
 const MainCTA = () => {
   return (
     <>
-
+      <section id='mainCta'>
+        <div className="container">
+          <h2>
+            Pridajte Sa<br></br>
+            K Nám!
+          </h2>
+          <CTA />
+        </div>
+      </section>
     </>
   )
 }
